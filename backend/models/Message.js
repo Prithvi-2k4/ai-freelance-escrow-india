41ec94d6db40ca2303c5be4385dfdf466a27c6c7
Prithvi-2k4 <mprithvikrish@gmail.com>
2025-12-05 16:59:22 +0100

    Fix formatting issue in Message.js

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -3,5 +3,5 @@ const msgSchema = new mongoose.Schema({
   room: String,
   sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   text: String
-},{ timestamps:true });
+}, { timestamps:true });
 module.exports = mongoose.model('Message', msgSchema);
