1250afad7775069bc5d40ac0b5ff82521c0a7b41
Prithvi-2k4 <mprithvikrish@gmail.com>
2025-12-05 14:13:13 +0100

    Refactor App component with ProtectedRoute and new layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,33 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
 import JobsList from './pages/JobsList';
 import PostJob from './pages/PostJob';
-import JobDetail from './pages/JobDetail';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
 import Navbar from './components/Navbar';
+import ProtectedRoute from './components/ProtectedRoute';
 
 export default function App(){
-  return <BrowserRouter>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<JobsList/>}/>
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/dashboard" element={<Dashboard/>}/>
-      <Route path="/post" element={<PostJob/>}/>
-      <Route path="/job/:id" element={<JobDetail/>}/>
-    </Routes>
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <div className="min-h-screen">
+        <Navbar />
+        <main className="max-w-5xl mx-auto p-6">
+          <Routes>
+            <Route path="/" element={<JobsList />} />
+            <Route path="/jobs" element={<JobsList />} />
+            <Route path="/post" element={
+              <ProtectedRoute><PostJob/></ProtectedRoute>
+            } />
+            <Route path="/dashboard" element={
+              <ProtectedRoute><Dashboard/></ProtectedRoute>
+            } />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/register" element={<Register/>} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
 }
