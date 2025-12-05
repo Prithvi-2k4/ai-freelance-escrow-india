657169c74c4c230c6ed5adfda11ec9f7aee842ba
Prithvi-2k4 <mprithvikrish@gmail.com>
2025-12-05 16:19:22 +0100

    Create Notification model with schema

---
+++ backend/models/Notification.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const notifSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  title: String,
+  body: String,
+  read: { type: Boolean, default:false },
+  meta: {}
+},{ timestamps:true });
+module.exports = mongoose.model('Notification', notifSchema);
