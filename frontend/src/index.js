4ddd8cf21b4ad22bb452d27cca0e3fc156df12e2
Prithvi-2k4 <mprithvikrish@gmail.com>
2025-12-07 22:57:46 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(<App />);
