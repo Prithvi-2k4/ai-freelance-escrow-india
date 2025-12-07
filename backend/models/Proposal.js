6795e3ef7e01faf87dae5c902901efe40fdb58c6
Prithvi-2k4 <mprithvikrish@gmail.com>
2025-12-07 22:18:19 +0100

    Update Proposal.js

--- backend/models/Proposal.js
+++ backend/models/Proposal.js
@@ -1,10 +1,11 @@
-// backend/models/Proposal.js
 const mongoose = require('mongoose');
-const proposalSchema = new mongoose.Schema({
+
+const ProposalSchema = new mongoose.Schema({
   job: { type: mongoose.Schema.Types.ObjectId, ref: 'Job' },
   freelancer: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   cover: String,
   bid: Number,
-  status: { type: String, enum: ['pending','accepted','rejected'], default: 'pending' }
-}, { timestamps:true });
-module.exports = mongoose.model('Proposal', proposalSchema);
+  status: { type: String, default: 'pending' },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Proposal', ProposalSchema);
