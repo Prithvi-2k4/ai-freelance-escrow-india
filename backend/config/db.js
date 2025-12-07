c5a1f90850f5410c85c8cf9ae3b6259d5bdd8882
Prithvi-2k4 <mprithvikrish@gmail.com>
2025-12-07 22:17:04 +0100

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
-module.exports = async () => {
+
+const connectDB = async () => {
+  if (!process.env.MONGO_URI) throw new Error('MONGO_URI not set');
   await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true, useUnifiedTopology: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   });
-  console.log('MongoDB connected');
+  return mongoose.connection;
 };
+
+module.exports = connectDB;
