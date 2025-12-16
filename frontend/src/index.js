cb507d7a4281f32bc112f49780bb59865fba5577
Prithvi-2k4 <mprithvikrish@gmail.com>
2025-12-16 02:05:54 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';   // 🔥 THIS LINE FIXES EVERYTHING
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
