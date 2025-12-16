deee33c14af94774987e552f5cc3fa1ab8255d38
Prithvi-2k4 <mprithvikrish@gmail.com>
2025-12-16 11:05:58 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,24 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
+const connectDB = require('./config/db');
 
 const authRoutes = require('./routes/auth');
+const jobRoutes = require('./routes/jobs');
 
 const app = express();
 
-// middleware
+connectDB();
+
 app.use(cors());
-app.use(express.json()); // VERY IMPORTANT
+app.use(express.json());
 
-// routes
 app.use('/api/auth', authRoutes);
+app.use('/api/jobs', jobRoutes);
 
-// db + server start
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log('MongoDB connected');
-    app.listen(process.env.PORT || 5000, () =>
-      console.log('Server running')
-    );
-  })
-  .catch(err => console.error(err));
+app.get('/', (req, res) => {
+  res.send('API running');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
